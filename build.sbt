4d5a8f355c0b98c5a59b20ff2936ae2acadae548
Francisco Fernández Castaño <fr.fernandez@flowlab.biz>
2013-05-07 16:33:38 +0200

    Using akka

--- build.sbt
+++ build.sbt
@@ -2,4 +2,10 @@ name := "rabbit-queues"
 
 version := "1.0"
 
+scalaVersion := "2.10.1"
+
+resolvers += "Typesafe Snapshots" at "http://repo.typesafe.com/typesafe/snapshots/"
+
 libraryDependencies += "com.rabbitmq" % "amqp-client" % "2.8.1"
+
+libraryDependencies += "com.typesafe.akka" % "akka-actor_2.10" % "2.2-SNAPSHOT"
--- src/main/scala/rabbit/queue.scala
+++ src/main/scala/rabbit/queue.scala
@@ -1,35 +1,108 @@
 package rabbit
 
+import akka.actor.Actor
+import akka.actor.ActorRef
+import akka.dispatch.Filter
 import com.rabbitmq.client._
+import akka.actor.ActorLogging
+import akka.actor.ActorSystem
+import akka.actor.Props
+import akka.routing.SmallestMailboxRouter
+
+
+case class Greeting(who: String)
+
+class GreetingActor extends Actor with ActorLogging{
+  var acc = 0;
+  def receive = {
+    case Greeting(who) =>
+      acc += 1
+      log.info("Hello" + who)
+
+  }
+}
+
+object RabbitMQConnection {
+
+  private val connection: Connection = null;
+
+  /**
+   * Return a connection if one doesn't exist. Else create
+   * a new one
+   */
+  def getConnection(): Connection = {
+    connection match {
+      case null => {
+        val factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.newConnection();
+      }
+      case _ => connection
+    }
+  }
+}
+
+
+class MsgConsumer(channel: Channel, target: ActorRef) extends DefaultConsumer(channel){
+
+  override def handleDelivery(consumer_tag: String,
+                              envelope: Envelope,
+                              properties: AMQP.BasicProperties,
+                              body: Array[Byte]): Unit =
+  {
+    val delivery_tag = envelope.getDeliveryTag
+    val body_text = new String(body)
+    try {
+      val msg = body_text
+      target ! Greeting(msg)
+      channel.basicAck(delivery_tag, false)
+    } catch {
+      case e: Exception =>
+        channel.basicReject(delivery_tag, false)
+    }
+
+  }
+}
 
 object Rabbit {
   def main(args: Array[String]){
 
-    val factory = new ConnectionFactory()
-      factory.setHost("localhost")
-    val connection = factory.newConnection
+    val connection = RabbitMQConnection.getConnection
     val channel = connection.createChannel
-
     channel.queueDeclare("task_queue", true, false, false, null)
-
     channel.basicQos(1)
 
-    val consumer = new QueueingConsumer(channel)
-      channel.basicConsume("task_queue", false, consumer)
+    val system = ActorSystem("MySystem")
 
-    while(true){
-      var delivery = consumer.nextDelivery
-      var message = new String(delivery.getBody())
-      println(message)
-      doWork(message)
 
-      channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false)
-    }
+    val event_filter = system.actorOf(
+      Props[GreetingActor],
+      name="EventFilter"
+    )
+
+    channel.basicConsume(
+      "task_queue",
+      false, // do not auto ack
+      new MsgConsumer(channel, event_filter)
+    )
+
+
+
+
+    //val consumer = new QueueingConsumer(channel)
+    //channel.basicConsume("task_queue", false, consumer)
+
+    //while(true){
+    //var delivery = consumer.nextDelivery
+    //var message = new String(delivery.getBody())
+    //println(message)
+    //doWork(message)
+
+    //channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false)
+    //}
   }
 
   def doWork(str: String){
-   Thread.sleep(9000)
+    Thread.sleep(9000)
   }
 }
-
-// vim: set ts=2 sw=2 et:
